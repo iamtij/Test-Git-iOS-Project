9a2927a762a43807fcb189a8620b83aab5501beb
TJ <tjtalusan@gmail.com>
2014-02-09 14:59:10 +0800

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by TJ Talusan on 2/9/14.
+//  Copyright (c) 2014 TJ Talusan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by TJ Talusan on 2/9/14.
+//  Copyright (c) 2014 TJ Talusan. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
